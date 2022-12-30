185e170364038f2a494a30a5de4bc39927476a1b
Anderson Vieira <anderson.vieira-terceiro@cea.com.br>
2022-12-30 14:48:37 -0300

    feat: added signin info dialog

--- app/src/main/java/com/tokenverse/wallet/sample/App.kt
+++ app/src/main/java/com/tokenverse/wallet/sample/App.kt
@@ -1,4 +1,14 @@
 package com.tokenverse.wallet.sample
 
-class App {
+import android.app.Application
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+import com.tokenverse.wallet.CrashListener
+import com.tokenverse.wallet.Wallet
+
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
 }
--- app/src/main/java/com/tokenverse/wallet/sample/MainActivity.kt
+++ app/src/main/java/com/tokenverse/wallet/sample/MainActivity.kt
@@ -2,10 +2,39 @@ package com.tokenverse.wallet.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import androidx.core.content.ContextCompat
+import com.tokenverse.wallet.dialog.*
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+        private lateinit var rbSigninInfoLight: RadioButton
+        private lateinit var rbSigninInfoDark: RadioButton
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+            rbSigninInfoLight = findViewById(R.id.rg_signinInfo_light)
+            rbSigninInfoDark = findViewById(R.id.rg_signinInfo_dark)
+        }
+
+        override fun onClick(view: View) {
+            when (view.id) {
+                R.id.btn_signin_info -> {
+                    val headerColor = ContextCompat.getColor(this, R.color.dialogHeaderColor)
+                    val buttonColor = ContextCompat.getColor(this, R.color.dialogHeaderColor)
+
+                    WalletDialog.Builder(this, WalletDialogStyle.SIGNIN_INFO, WalletDialogType.INFO)
+                        .setDarkMode(rbSigninInfoDark.isChecked)
+                        .setAnimation(WalletDialogAnimation.DEFAULT)
+                        .setOnClickListener(object : WalletDialogOnClickListener {
+                            override fun onClick(dialog: WalletDialog.Builder) {
+                                dialog.dismiss()
+                            }
+                        })
+                        .show()
+                }
+            }
+        }
     }
-}
